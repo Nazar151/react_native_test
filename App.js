9889535fbc8677e41a79a9849463d095384c9462
Nazar Volkov <nazar.volkov@gmail.com>
2021-04-28 22:25:23 +0200

    add todoList + css

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
+
+let todoList  =  [
+  {id: 1, title: "do task #1", body: 'lorem ipsum dolor sit ameti', priority: 'high', isComplete: false},
+  {id: 2, title: "do task #2", body: 'lorem ipsum dolor sit ameti', priority: 'high', isComplete: false},
+  {id: 3, title: "do task #3", body: 'lorem ipsum dolor sit ameti', priority: 'high', isComplete: false},
+  {id: 4, title: "do task #4", body: 'lorem ipsum dolor sit ameti', priority: 'high', isComplete: false},
+  {id: 6, title: "do task #5", body: 'lorem ipsum dolor sit ameti', priority: 'high', isComplete: false},
+  {id: 7, title: "do task #6", body: 'lorem ipsum dolor sit ameti', priority: 'high', isComplete: false}
+]
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+<FlatList
+    data={todoList}
+    renderItem={ ({item}) => {
+      console.log(item)
+      return <View style={styles.item}>
+          <Text>{item.title} - {item.isComplete.toString() }</Text>
+      </View>
+    }}
+    // keyExtractor={}
+/>
     </View>
   );
 }
@@ -18,4 +36,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+    item: {
+      height: 200,
+        marginTop: 10,
+        backgroundColor: 'silver',
+        border: 1,
+    }
 });
